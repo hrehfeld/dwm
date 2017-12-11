28e735587489df7b3d7a6961baf3c4be724a0a30
hrehfeld <hauke@haukerehfeld.de>
2017-12-11 21:20:07 +0100

    add: rules

--- config.h
+++ config.h
@@ -21,6 +21,9 @@ static const char *colors[][3]      = {
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
+
+#define TAG_NUM(n) (n-1)
+
 static const Rule rules[] = {
 	/* xprop(1):
 	 *	WM_CLASS(STRING) = instance, class
@@ -28,8 +31,12 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "Firefox",  NULL,       NULL,       1 << TAG_NUM(9),       0,           -1 },
+	{ "Google-chrome",  NULL, NULL,       1 << TAG_NUM(6),       0,           -1 },
+	{ "Nextcloud", "nextcloud", NULL,     1 << TAG_NUM(9),       0,           -1 },
 };
+
+#undef TAG_NUM
 
 /* layout(s) */
 static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
